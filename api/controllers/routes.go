53e4e6c74d65e38add0b628d19c0ac9675fc7105
Arman <hossain.arman@northsouth.edu>
2022-09-22 18:45:33 +0600

    udpate

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -4,25 +4,25 @@ import (
 	"github.com/MD-ARMAN-Shanto/gostack/api/middlewares"
 )
 
-func (server *Server) initializeRoutes() {
+func (s *Server) initializeRoutes() {
 
 	// Home routes
-	server.Router.HandleFunc("/", middlewares.SetMiddlewareJson(server.Home)).Methods("GET")
+	s.Router.HandleFunc("/", middlewares.SetMiddlewareJson(s.Home)).Methods("GET")
 
 	// Login Route
-	server.Router.HandleFunc("/login", middlewares.SetMiddlewareJson(server.Login)).Methods("POST")
+	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJson(s.Login)).Methods("POST")
 
 	// user Routes
-	server.Router.HandleFunc("/users", middlewares.SetMiddlewareJson(server.CreateUser)).Methods("POST")
-	server.Router.HandleFunc("/users", middlewares.SetMiddlewareJson(server.GetUsers)).Methods("GET")
-	server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(server.GetUser)).Methods("GET")
-	server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(middlewares.SetMiddlewareAuthentication(server.UpdateUser))).Methods("PUT")
-	server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(server.DeleteUser)).Methods("DELETE")
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJson(s.CreateUser)).Methods("POST")
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJson(s.GetUsers)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(s.GetUser)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(middlewares.SetMiddlewareAuthentication(s.UpdateUser))).Methods("PUT")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJson(s.DeleteUser)).Methods("DELETE")
 
 	// post Routes
-	server.Router.HandleFunc("/posts", middlewares.SetMiddlewareJson(server.CreatePost)).Methods("POST")
-	server.Router.HandleFunc("/posts", middlewares.SetMiddlewareJson(server.GetPosts)).Methods("GET")
-	server.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(server.GetPost)).Methods("GET")
-	server.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(middlewares.SetMiddlewareAuthentication(server.UpdatePost))).Methods("PUT")
-	server.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(server.DeletePost)).Methods("DELETE")
+	s.Router.HandleFunc("/posts", middlewares.SetMiddlewareJson(s.CreatePost)).Methods("POST")
+	s.Router.HandleFunc("/posts", middlewares.SetMiddlewareJson(s.GetPosts)).Methods("GET")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(s.GetPost)).Methods("GET")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(middlewares.SetMiddlewareAuthentication(s.UpdatePost))).Methods("PUT")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJson(s.DeletePost)).Methods("DELETE")
 }
